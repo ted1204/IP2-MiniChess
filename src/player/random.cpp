c326e758a07175f5bb52cfe3fa19ad31fe0c2b99
ted1204 <120557338+ted1204@users.noreply.github.com>
2023-06-20 16:10:12 +0800

    depth_begin_from_4

--- src/player/random.cpp
+++ src/player/random.cpp
@@ -40,14 +40,16 @@ void read_board(std::ifstream& fin) {
  */
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
+  int depth = 4;
   while(true) {
     // Choose a random spot.
-    auto move = Random::get_move(root, 0);
+    auto move = Random::get_move(root, depth);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
     // Remember to flush the output to ensure the last action is written to file.
     fout.flush();
+    depth += 1;
     break;
   }
 }
